a556f0bd0621b5116423ce5c2e652ed7f342cae2
Bymarium <marianamejia.91619@gmail.com>
2022-11-14 16:22:57 -0500

    Terminada la sección de reglas de juego a travÃéde una alerta.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,36 +1,68 @@
-const navMenu   = document.getElementById('navbar-menu'),
-      navToggle = document.getElementById('navbar-toggle'),
-      navClose  = document.getElementById('navbar-close');
+const navMenu = document.getElementById("navbar-menu"),
+  navToggle = document.getElementById("navbar-toggle"),
+  navClose = document.getElementById("navbar-close");
 
-if(navToggle) {
-  navToggle.addEventListener('click', () => {
-    navMenu.classList.add('navbar__menu--visible');
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("navbar__menu--visible");
   });
 }
 
 if (navClose) {
-  navClose.addEventListener('click', () => {
-    navMenu.classList.remove('navbar__menu--visible');
-  })
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("navbar__menu--visible");
+  });
 }
 
-const navLinks = document.querySelectorAll('.navbar__link');
+const navLinks = document.querySelectorAll(".navbar__link");
 
 const handleClickLink = () => {
-  navMenu.classList.remove('navbar__menu--visible');
-}
+  navMenu.classList.remove("navbar__menu--visible");
+};
 
-navLinks.forEach(link => link.addEventListener('click', handleClickLink));
- 
+navLinks.forEach((link) => link.addEventListener("click", handleClickLink));
+
 const scrollHeader = () => {
-  const header = document.getElementById('header');
+  const header = document.getElementById("header");
   this.scrollY >= 50
-    ? header.classList.add('header--scrolling')
-    : header.classList.remove('header--scrolling');
+    ? header.classList.add("header--scrolling")
+    : header.classList.remove("header--scrolling");
+};
+
+window.addEventListener("scroll", scrollHeader);
+
+const alertH = document.getElementById("rulesh");
+if (alertH) {
+  alertH.addEventListener("click", () => {
+    Swal.fire({
+      title: "Reglas de juego",
+      html: "1. El juego consta de 10 preguntas <br/> 2. Disponerá de 60 segundos para cada pregunta <br/> 3. Cada pregunta tendrá dos opciones de respuestas <br/> 4. Deberá seleccionar la que considere correcta y darle click en el boton de enviar <br/> 5. Si la respuesta es correcta se marcará en verde, de lo contrario, se marcará en rojo <br/> 6. Al finalizar el juego se le dará un puntaje que determina si ganó o perdió el juego de preguntas",
+      confirmButtonText: "Entendido",
+      confirmButtonColor: "#ff289b",
+      showClass: {
+        popup: "animate__animated animate__fadeInDown",
+      },
+      hideClass: {
+        popup: "animate__animated animate__fadeOutUp",
+      },
+    });
+  });
 }
 
-window.addEventListener('scroll', scrollHeader);
-
-
-
-
+const alertF = document.getElementById("rulesf");
+if (alertF) {
+  alertF.addEventListener("click", () => {
+    Swal.fire({
+      title: "Reglas de juego",
+      html: "1. El juego consta de 10 preguntas <br/> 2. Disponerá de 60 segundos para cada pregunta <br/> 3. Cada pregunta tendrá dos opciones de respuestas <br/> 4. Deberá seleccionar la que considere correcta y darle click en el boton de enviar <br/> 5. Si la respuesta es correcta se marcará en verde, de lo contrario, se marcará en rojo <br/> 6. Al finalizar el juego se le dará un puntaje que determina si ganó o perdió el juego de preguntas",
+      confirmButtonText: "Entendido",
+      confirmButtonColor: "#ff289b",
+      showClass: {
+        popup: "animate__animated animate__fadeInDown",
+      },
+      hideClass: {
+        popup: "animate__animated animate__fadeOutUp",
+      },
+    });
+  });
+}
